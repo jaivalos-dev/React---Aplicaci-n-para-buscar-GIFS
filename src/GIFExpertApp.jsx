05946efc160e3883b78140be9b53895f55b44d4c
Javier Avalos <javalos@ccg.gt>
2023-11-28 16:51:42 -0600

    Creacion-del-proyecto

---
+++ src/GIFExpertApp.jsx
@@ -0,0 +1,9 @@
+
+
+export const GIFExpertApp = () => {
+  return (
+    <>
+        <h1>GIF Expert App</h1>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GIFExpertApp } from './GIFExpertApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GIFExpertApp />
+  </React.StrictMode>,
+)
