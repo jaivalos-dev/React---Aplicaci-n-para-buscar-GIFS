52a56c6d8228f95c8edc5777dc20a50c4e653817
Javier Avalos <javalos@ccg.gt>
2023-11-29 11:36:28 -0600

    Creamos-listas-de-categorias

--- src/GIFExpertApp.jsx
+++ src/GIFExpertApp.jsx
@@ -1,9 +1,23 @@
-
+import { useState } from "react"
 
 export const GIFExpertApp = () => {
+
+  const [categories, setCategories] = useState(["NBA"])  
+
+  console.log(categories)
+
   return (
     <>
         <h1>GIF Expert App</h1>
+
+        <ol>
+            { 
+                categories.map(category => {
+                    return <li key = {category}>{ category }</li>
+                }) 
+            }
+        </ol>
+
     </>
   )
 }
