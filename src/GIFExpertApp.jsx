94ddc09928311ba8d808d9cf5774f0617ad18b63
Javier Avalos <javalos@ccg.gt>
2023-11-29 13:44:51 -0600

    Creamos-componente-para-agregar-categorias-desde-un-textbox

--- src/GIFExpertApp.jsx
+++ src/GIFExpertApp.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
 
 export const GIFExpertApp = () => {
 
-  const [categories, setCategories] = useState(["NBA"])  
+  const [categories, setCategories] = useState([])  
 
-  console.log(categories)
+  const onAddCategory = () => {
+    setCategories(["Valorant", ...categories])
+  }
 
   return (
     <>
         <h1>GIF Expert App</h1>
+
+        {/* Input */}
+
+        <AddCategory/>
+
+        <button onClick={onAddCategory}>Agregar</button>
 
         <ol>
             { 
